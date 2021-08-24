06da0f054820bf8132b71c195786b5cc3f8db3da
varshini <dreamvarshini@gmail.com>
2021-08-24 22:55:33 +0530

    ADDED:Finding square root program

---
+++ src/com/bridgelabz/logicalprograms/SquareRoot.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.logicalprograms;
+
+import java.util.Scanner;
+
+public class SquareRoot 
+{
+
+	public static void main(String[] args) 
+	{
+		double c;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter value to get square root");
+		c = scanner.nextDouble();
+
+		double t = c;
+		double epsilon = 1.0e-15;
+		while (Math.abs(t - c / t) > epsilon * t) 
+		{
+			t = (c / t + t) / 2.0;
+		}
+
+		System.out.println("square root of " + c + " is " + t);
+	     
+		scanner.close();
+
+	}
+
+}
